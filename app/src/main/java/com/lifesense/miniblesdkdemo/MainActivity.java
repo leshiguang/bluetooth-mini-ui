ac01ba2d00eff18b113280048f01d0d82a38037c
shangyifei <46328104@qq.com>
2021-01-19 21:35:01 +0800

    修复心率开关设置无效、增加读取来电状态权限

--- app/src/main/java/com/lifesense/miniblesdkdemo/MainActivity.java
+++ app/src/main/java/com/lifesense/miniblesdkdemo/MainActivity.java
@@ -5,10 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import com.lifesense.android.health.service.common.ui.BaseActivity;
 import com.lifesense.android.health.service.deviceconfig.ui.DeviceStatusListActivity;
+import com.lifesense.android.health.service.util.ToastUtil;
 import com.lifesense.utils.ImageUtil;
 
 public class MainActivity extends BaseActivity {
@@ -32,16 +31,29 @@ public class MainActivity extends BaseActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 200) {
-            if (checkPermission()) {
-                startActivity(new Intent(this, DeviceStatusListActivity.class));
-                finish();
+            if(grantResults != null) {
+                boolean flag = true;
+                String leakPermission = "";
+                for (int i = 0;i < grantResults.length; i++) {
+                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        leakPermission = permissions[i];
+                        flag = false;
+                    }
+                }
+                if(flag) {
+                    startActivity(new Intent(this, DeviceStatusListActivity.class));
+                    finish();
+                } else {
+                    ToastUtil.showCenterShowToast(this,"缺少权限:" + leakPermission);
+                }
             }
+
         }
     }
 
 
     private boolean checkPermission() {
-        return checkReadPermission(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.READ_SMS,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.BLUETOOTH, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        return checkReadPermission(new String[]{Manifest.permission.READ_PHONE_STATE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.READ_SMS,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.BLUETOOTH, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, 200);
     }
 
 }
